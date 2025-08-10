697389b24de54b98f005282199fe9ea58706681b
Keerthi Gollapalli <keerthigollapalli@Mac.fritz.box>
2025-08-10 13:59:31 +0200

    fix: replace system theme with light mode

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
 	headerTitle: 'Creating Wireframes to Wonders',
 	description: 'Crafting intuitive and impactful digital experiences that connect users with solutions seamlessly.',
 	language: 'en-us',
-	theme: 'system', // system, dark or light
+	theme: 'light', // system, dark or light
 	siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
 	siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
 	siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
